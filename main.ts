f3b79f0bf711da3e6b0e79db84d7a6af28de5357
deepgem <202297758+deepgem@users.noreply.github.com>
2026-04-05 11:37:20 +0800

    Modify font-related settings

--- main.ts
+++ main.ts
@@ -793,7 +793,7 @@ class TerminalSession {
       fontSize: 14,
       lineHeight: 1.4,
       letterSpacing: 0.3,
-      fontFamily: "'PingFang SC', 'JetBrainsMono Nerd Font'",
+      fontFamily: "'Sarasa Fixed SC', 'JetBrainsMono Nerd Font'",
       fontWeight: "400",
       fontWeightBold: "600",
       theme: getObsidianTheme(),
